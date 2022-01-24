b3060d65880cd3c767d5dd796099d57188cd6824
2002sarika <71333456+2002sarika@users.noreply.github.com>
2022-01-24 11:54:33 +0530

    Update settings.py

--- Customer/settings.py
+++ Customer/settings.py
@@ -16,14 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2%wtown7r06bjdp3$t0udsl9z1goo3j+1=%oxi0g#_tn28q9t_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
